e320f789261ffaf3bc3b758d03ebf714d02f1a55
Xemay <settarovaf7@gmail.com>
2026-01-21 22:38:11 +0300

    Second commit

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     unoptimized: true,
   },
   // Раскомментируй строку ниже и замени 'your-repo-name' на имя твоего репозитория
-  // basePath: '/your-repo-name',
+  basePath: '/Xemay',
 }
 
 export default nextConfig
